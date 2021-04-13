a701ac8813bb5da94425c357cca23dc58fdf30da
Will Kofski <will.kofski@gmail.com>
2021-04-13 19:14:22 -0400

    final bug fix

--- pizza-app/src/app/app.module.ts
+++ pizza-app/src/app/app.module.ts
@@ -8,13 +8,7 @@ import { OrdersComponent } from "./orders/orders.component";
 import { ItemsComponent } from "./items/items.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-<<<<<<< HEAD
 import { TimeAgoPipe } from "time-ago-pipe";
-=======
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { MatButtonModule } from "@angular/material/button";
-import { MatFormFieldModule } from "@angular/material/form-field";
->>>>>>> 7b34df46ee2642228d3bca176a89c370719cd03c
 
 @NgModule({
   declarations: [
@@ -30,9 +24,6 @@ import { MatFormFieldModule } from "@angular/material/form-field";
     ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
-    NoopAnimationsModule,
-    MatButtonModule,
-    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
